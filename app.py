a1e2774f208fb8fb19960e2d0d755a82ab1c4b10
nijikaraka <jmffn317@gmail.com>
2024-08-28 22:55:16 +0900

    速度を向上

--- app.py
+++ app.py
@@ -1,64 +1,59 @@
-from flask import Flask, render_template
-from flask import request
+from quart import Quart, render_template, request
 import datetime
 import get_comment_by_id
 import search_youtube
 import common
-# from flask import Markup
 
-app = Flask(__name__, static_folder='./templates/images')
+app = Quart(__name__, static_folder='./templates/images')
 
 @app.route('/')
-def hello():
-    return render_template('layout.html', title='search_youtube')
+async def hello():
+    return await render_template('layout.html', title='search_youtube')
 
-# ↓ /scrapingをGETメソッドで受け取った時の処理
+# /scrapingをGETメソッドで受け取った時の処理
 @app.route('/scraping', methods=['GET', 'POST'])
-def get():
-    channel_id = request.args.get("channel-id","")
-    word = request.args.get("word","")
-    from_date = request.args.get("from","")
-    to_date = request.args.get("to","")
-    viewcount_level = int(request.args.get("viewcount-level","") or 0)
-    subscribercount_level = int(request.args.get("subscribercount-level","") or 0)
-    video_count = int(request.args.get("video-count","") or 0)
-    is_get_comment = request.args.get("comments","")
-    sorce = search_youtube.search_youtube(channel_id,word,from_date,to_date,viewcount_level,subscribercount_level,video_count,is_get_comment)
-    # print(sorce)
+async def get():
+    channel_id = request.args.get("channel-id", "")
+    word = request.args.get("word", "")
+    from_date = request.args.get("from", "")
+    to_date = request.args.get("to", "")
+    viewcount_level = int(request.args.get("viewcount-level", "") or 0)
+    subscribercount_level = int(request.args.get("subscribercount-level", "") or 0)
+    video_count = int(request.args.get("video-count", "") or 0)
+    is_get_comment = request.args.get("comments", "")
+    
+    # 非同期関数を呼び出すためにawaitを使用
+    sorce = await search_youtube.search_youtube(channel_id, word, from_date, to_date, viewcount_level, subscribercount_level, video_count, is_get_comment)
+    
     print(datetime.datetime.now())
-    if sorce == None:
-        return render_template('layout.html', title='search_youtube')
+    if sorce is None:
+        return await render_template('layout.html', title='search_youtube')
 
     if request.method == 'GET': # GETされたとき
         print('出力')
-        # sorce = Markup(sorce)
-        return render_template('template.html',sorce = sorce,is_get_comment = is_get_comment)
+        return await render_template('template.html', sorce=sorce, is_get_comment=is_get_comment)
         
     elif request.method == 'POST': # POSTされたとき
         return 'POST'
 
-# ↓ /commentをGETメソッドで受け取った時の処理
+# /commentをGETメソッドで受け取った時の処理
 @app.route('/comment', methods=['GET', 'POST'])
-def get_comment():
-    video_id = common.get_video_id(request.args.get("video-id",""))
-    # リクエストからvideo-idを取得
-
-    print(video_id)
-    sorce = get_comment_by_id.get_comment_by_id(video_id,'') 
-    # print(sorce)
+async def get_comment():
+    video_id = common.get_video_id(request.args.get("video-id", ""))
+    
+    # 非同期関数を呼び出すためにawaitを使用
+    sorce = await get_comment_by_id.get_comment_by_id(video_id, '')
+    
     print(datetime.datetime.now())
-    if sorce == None:
-        return render_template('layout.html', title='search_youtube')
+    if sorce is None:
+        return await render_template('layout.html', title='search_youtube')
 
     if request.method == 'GET': # GETされたとき
         print('出力')
-        # sorce = Markup(sorce)
-        # print(sorce)
-        return render_template('comment.html',sorce = sorce)
+        return await render_template('comment.html', sorce=sorce)
         
     elif request.method == 'POST': # POSTされたとき
         return 'POST'
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- search_youtube.py
+++ search_youtube.py
@@ -1,14 +1,15 @@
-import urllib.request
-import urllib.parse
-import requests
+import aiohttp
+import asyncio
 import json
 import datetime
 import common
 import os
 from dotenv import load_dotenv
+import urllib.parse
+import requests
+import get_comment
 
-#-------↓パラメータ入力↓-------
-def search_youtube(channel_id,key_word,published_from,published_to,viewcount_level,subscribercount_level,video_count,is_get_comment):
+async def search_youtube(channel_id, key_word, published_from, published_to, viewcount_level, subscribercount_level, video_count, is_get_comment):
     # 開始時刻
     print(datetime.datetime.now())
     load_dotenv('.env') 
@@ -20,289 +21,173 @@ def search_youtube(channel_id,key_word,published_from,published_to,viewcount_lev
 
     url = os.environ.get("URL")
     api_key = os.environ.get("API_KEY")
-    # key_word = 'ゲーム'
+    
     regionCode = 'JP'
-    # 検索日時from
     published_from += 'T00:00:00.000Z'
-    # # 検索日時to
     published_to += 'T23:59:59.999Z'
     videoCategoryId = ''
-    comment = []
-    
-    # 再生数下限
-    # viewcount_level = 0
-    # # 登録者数上限
-    # subscribercount_level = 100000000
-    # # 取得件数
-    # video_count = 10000
-
-    #-------↑パラメータ入力↑-------
-
     nextPageToken = ''
     buzz_lists_count = []
     outputs = []
 
-    #無限ループ
-    while True:
-        buzz_lists = []
-        #searchメソッドで動画ID一覧取得
-        param = {
-            'part':'snippet',
-            'q':key_word,
-            'regionCode':regionCode,
-            'maxResults':50,
-            'order':'viewcount',
-            'publishedAfter':published_from,
-            'publishedBefore':published_to,
-            'type':'video',
-            'channelId':channel_id,
-            'pageToken':nextPageToken,
-            'key':api_key
-        }
-        #videoCategoryIdはブランクでパラメータを渡すとエラーになるので値がある時のみパラメータ付ける
-        if not videoCategoryId:
-            pass
-        else:
-            param['videoCategoryId'] = videoCategoryId
-        #target_url:youtubeからのjson情報
-        target_url = url + 'search?'+urllib.parse.urlencode(param)
-        # print(target_url)
-        req = urllib.request.Request(target_url)
-        try:
-            with urllib.request.urlopen(req) as res:
+    async with aiohttp.ClientSession() as session:
+        while True:
+            buzz_lists = []
+            param = {
+                'part': 'snippet',
+                'q': key_word,
+                'regionCode': regionCode,
+                'maxResults': 50,
+                'order': 'viewcount',
+                'publishedAfter': published_from,
+                'publishedBefore': published_to,
+                'type': 'video',
+                'channelId': channel_id,
+                'pageToken': nextPageToken,
+                'key': api_key
+            }
+            if videoCategoryId:
+                param['videoCategoryId'] = videoCategoryId
 
-                search_body = json.load(res)
-                video_list = []
-                channels_list = []
-
-                for item in search_body['items']:
-                    #videoメソッド用list作成
-                    video_list.append(item['id']['videoId'])
-                    channels_list.append(item['snippet']['channelId'])
-                    #出力用データに追記
-                    buzz_lists.append( {'videoId':item['id']['videoId'], 'channelId':item['snippet']['channelId']} )
+            target_url = url + 'search?' + urllib.parse.urlencode(param)
+            try:
+                async with session.get(target_url) as response:
+                    # ステータスコードをチェック
+                    if response.status != 200:
+                        error_message = f"API request failed with status {response.status}: {await response.text()}"
+                        return [
+                        {
+                            'error': error_message
+                        }]
+                    search_body = await response.json()
+                    video_list = [item['id']['videoId'] for item in search_body['items']]
+                    channels_list = [item['snippet']['channelId'] for item in search_body['items']]
                     
-                #videoメソッドで動画情報取得-----------------------------------------------------------------
-                if get_video(url,buzz_lists,video_list,api_key) == False:
-                    break
+                    # まずget_videoを実行し、それが完了した後にget_channelを呼び出す
+                    await get_video(session, url, buzz_lists, video_list, api_key)
+                    await get_channel(session, url, param, buzz_lists, channels_list, api_key)
                     
-                #channelメソッドで登録者数取得-----------------------------------------------------------------
-                if get_channel(url,param,buzz_lists,channels_list,api_key) == False:
-                    break
+                    # 非同期でURLのリダイレクトを確認
+                    video_urls = await get_video_urls(session, buzz_lists)
+                    
+                    # 条件を満たす buzz_list を一度に処理
+                    valid_buzz_lists = [
+                        {
+                            'publishedAt': common.change_time(buzz_list['publishedAt']),
+                            'title': buzz_list['title'], 
+                            'description': buzz_list['description'], 
+                            'viewCount': buzz_list['viewCount'], 
+                            'likeCount': buzz_list['likeCount'], 
+                            'commentCount': buzz_list['commentCount'], 
+                            'videoDuration': buzz_list['videoDuration'], 
+                            'thumbnails': buzz_list['thumbnails'], 
+                            'video_url': video_urls[i],
+                            'name': buzz_list.get('name', 'Unknown'), 
+                            'subscriberCount': buzz_list.get('subscriberCount', 0), 
+                            'channel_icon': [buzz_list.get('channel_url', ''), buzz_list.get('channel_icon', '')],
+                            'comment': (await get_comment(session, api_key, buzz_list['video_id'], nextPageToken) if is_get_comment else [])
+                        }
+                        for i, buzz_list in enumerate(buzz_lists)
+                        if int(buzz_list['viewCount']) >= viewcount_level and int(buzz_list['subscriberCount']) >= subscribercount_level
+                    ]
+                    buzz_lists_count.extend(valid_buzz_lists)
+                    outputs.extend(valid_buzz_lists)
 
-                #指定した再生回数以上 and 登録者数以下の場合のみCSVに吐く-----------------------------------------
+                print('出力結果'+str(len(buzz_lists_count))+'件')
+                if len(buzz_lists_count) >= video_count:
+                    return outputs
+
+                if 'nextPageToken' in search_body:
+                    nextPageToken = search_body['nextPageToken']
+                else:
+                    return outputs
+
+            except aiohttp.ClientResponseError as err:
+                print("エラー")
+                print(err)
+                break
+            except aiohttp.ClientConnectorError as err:
+                print("エラー")
+                print(err)
+                break
+
+async def get_video(session, url, buzz_lists, video_list, api_key):
+    param = {
+        'part': 'snippet,statistics,contentDetails',
+        'id': ",".join(video_list),
+        'key': api_key
+    }
+    target_url = url + 'videos?' + urllib.parse.urlencode(param)
+    try:
+        async with session.get(target_url) as response:
+            videos_body = await response.json()
+            for i, item in enumerate(videos_body['items']):
+                buzz_lists.append({})  # 空の辞書を追加しておく
+                buzz_lists[i]['channelId'] = item['snippet']['channelId'] 
+                buzz_lists[i]['title'] = item['snippet']['title'] 
+                buzz_lists[i]['description'] = item['snippet']['description']
+                buzz_lists[i]['viewCount'] = item['statistics']['viewCount']
+                buzz_lists[i]['publishedAt'] = item['snippet']['publishedAt']
+                buzz_lists[i]['thumbnails'] = item['snippet']['thumbnails']['high']['url']
+                buzz_lists[i]['likeCount'] = item['statistics'].get('likeCount', 0)
+                buzz_lists[i]['commentCount'] = item['statistics'].get('commentCount', 0)
+                buzz_lists[i]['videoDuration'] = common.get_time(common.parse_duration(item['contentDetails']['duration']))
+                buzz_lists[i]['video_id'] = item['id']
+
+    except aiohttp.ClientResponseError as err:
+        print('エラー')
+        print(err)
+        return False
+    except aiohttp.ClientConnectorError as err:
+        print('エラー')
+        print(err)
+        return False
+    return True
+
+async def get_channel(session, url, param, buzz_lists, channels_list, api_key):
+    param = {
+        'part': 'snippet,statistics',
+        'id': ",".join(channels_list),
+        'key': api_key
+    }
+    target_url = url + 'channels?' + urllib.parse.urlencode(param)
+
+    try:
+        async with session.get(target_url) as response:
+            channels_body = await response.json()
+            for i, buzz_list in enumerate(buzz_lists):
+                list_search = [item for item in channels_body['items'] if item['id'] == buzz_list['channelId']]
                 
-                for buzz_list in buzz_lists:
-                    # print(viewcount_level)
-                    # print(buzz_list['viewCount'])
-
-                    if( int(buzz_list['viewCount']) >= viewcount_level and int(buzz_list['subscriberCount']) >= subscribercount_level ):
-
-                        #ショート動画の存在チェック
-                        if not requests.get('https://www.youtube.com/shorts/' + buzz_list['video_id']).history:
-                            video_url = 'https://www.youtube.com/shorts/' + buzz_list['video_id']
-                        else:
-                            video_url = 'https://www.youtube.com/watch?v=' + buzz_list['video_id']
-
-                        if is_get_comment:
-                            # コメント取得
-                            print("comment")
-                            comment = get_comment(api_key,buzz_list['video_id'],nextPageToken)
-                        outputs.append({
-                            'publishedAt':common.change_time(buzz_list['publishedAt']),
-                            'title':buzz_list['title'], 
-                            'description':buzz_list['description'], 
-                            'viewCount':buzz_list['viewCount'], 
-                            'likeCount':buzz_list['likeCount'], 
-                            'commentCount':buzz_list['commentCount'], 
-                            'videoDuration':buzz_list['videoDuration'], 
-                            'thumbnails':buzz_list['thumbnails'], 
-                            'video_url':video_url, 
-                            'name':buzz_list['name'], 
-                            'subscriberCount':buzz_list['subscriberCount'], 
-                            'channel_icon':[buzz_list['channel_url'],buzz_list['channel_icon']],
-                            'comment':comment
-                        # comment 
-                        })
-                        #ループ数管理用
-                        buzz_lists_count.append(buzz_list)
-
-            print("roop")
-            #条件に合致する動画が必要数集まるまでループ-----------------------------------------
-            # print(len(buzz_lists_count))
-            if( len(buzz_lists_count) >= video_count ):
-                print('条件に合致する動画が必要数集まる')
-                return outputs
-
-            #nextPageTokenが表示されなくなったらストップ
-            if 'nextPageToken' in search_body:
-                nextPageToken = search_body['nextPageToken']
-            else:
-                print('nextPageTokenが表示されなくなった')
-                return outputs
-
-        except urllib.error.HTTPError as err:
-            print("HTTPError")
-            print(err)
-            break
-        except urllib.error.URLError as err:
-            print(err)
-            break
-
-# 動画情報取得
-def get_video(url,buzz_lists,video_list,api_key):
-    param = {
-        'part':'snippet,statistics,contentDetails',
-        'id':",".join(video_list),
-        'key':api_key
-    }
-    target_url = url + 'videos?'+(urllib.parse.urlencode(param))
-    # print(target_url)
-    req = urllib.request.Request(target_url)
-    try:
-        with urllib.request.urlopen(req) as res:
-            videos_body = json.load(res)
-
-            #出力用データに追記
-            v = 0
-
-            for item in videos_body['items']:
-                buzz_lists[v]['title'] = item['snippet']['title'] #タイトル
-                buzz_lists[v]['description'] = item['snippet']['description'] #概要
-                buzz_lists[v]['viewCount'] = item['statistics']['viewCount'] #再生数
-                buzz_lists[v]['publishedAt'] = item['snippet']['publishedAt'] #投稿日時
-                buzz_lists[v]['thumbnails'] = item['snippet']['thumbnails']['high']['url'] #サムネイル
-                # buzz_lists[v]['likeCount'] = int(item['statistics']['likeCount'] or 0) #高評価数
-                # なぜか高評価数がない場合がある
-                if 'likeCount' in item['statistics'] :
-                    buzz_lists[v]['likeCount'] = item['statistics']['likeCount'] #高評価数
+                if list_search:
+                    channel_info = list_search[0]
+                    buzz_list['name'] = channel_info['snippet']['title']
+                    buzz_list['subscriberCount'] = channel_info['statistics']['subscriberCount']
+                    buzz_list['channel_url'] = 'https://www.youtube.com/channel/' + channel_info['id']
+                    buzz_list['channel_icon'] = channel_info['snippet']['thumbnails']['default']['url']
                 else:
-                    buzz_lists[v]['likeCount'] = 0
-                # なぜかコメント数がない場合がある
-                if 'commentCount' in item['statistics'] :
-                    buzz_lists[v]['commentCount'] = item['statistics']['commentCount'] #コメント数
-                else:
-                    buzz_lists[v]['commentCount'] = 0
-                # print(common.parse_duration(item['contentDetails']['duration']))
-                buzz_lists[v]['videoDuration'] = common.get_time(common.parse_duration(item['contentDetails']['duration'])) #動画時間
-                buzz_lists[v]['video_id'] = item['id'] #id
-                v += 1
+                    print(f"Channel ID {buzz_list['channelId']} not found in response.")
 
-    except urllib.error.HTTPError as err:
+    except aiohttp.ClientResponseError as err:
         print(err)
         return False
-
-    except urllib.error.URLError as err:
+    except aiohttp.ClientConnectorError as err:
         print(err)
         return False
     return True
 
-# チャンネル情報取得
-def get_channel(url,param,buzz_lists,channels_list,api_key):
-    param = {
-        'part':'snippet,statistics',
-        'id':",".join(channels_list),
-        'key':api_key
-    }
-    target_url = url + 'channels?'+(urllib.parse.urlencode(param))
-    # print(target_url)
-    req = urllib.request.Request(target_url)
+async def get_video_urls(session, buzz_lists):
+    tasks = []
+    for buzz_list in buzz_lists:
+        shorts_url = 'https://www.youtube.com/shorts/' + buzz_list['video_id']
+        watch_url = 'https://www.youtube.com/watch?v=' + buzz_list['video_id']
+        tasks.append(check_redirect(session, shorts_url, watch_url))
 
-    try:
-        with urllib.request.urlopen(req) as res:
-            channels_body = json.load(res)
+    video_urls = await asyncio.gather(*tasks)
+    return video_urls
 
-            #出力用データに追記
-            c = 0
-            for buzz_list in buzz_lists:
-                list_search = [ item for item in channels_body['items'] if item['id'] == buzz_list['channelId'] ]
-                buzz_lists[c]['name'] = list_search[0]['snippet']['title'] #チャンネル名
-                buzz_lists[c]['subscriberCount'] = list_search[0]['statistics']['subscriberCount'] #登録者数
-                buzz_lists[c]['channel_url'] = 'https://www.youtube.com/channel/'+list_search[0]['id'] #チャンネルURL
-                buzz_lists[c]['channel_icon'] = list_search[0]['snippet']['thumbnails']['default']['url'] #チャンネルアイコン
-                c += 1
-
-    except urllib.error.HTTPError as err:
-        print(err)
-        return False
-    except urllib.error.URLError as err:
-        print(err)
-        return False
-    return True
-
-def print_video_comment(no, video_id, next_page_token):
-      
-  load_dotenv('.env') 
-
-  url = os.environ.get("URL")
-  api_key = os.environ.get("API_KEY")
-  params = {
-    'key': api_key,
-    'part': 'snippet',
-    'videoId': video_id,
-    'order': 'relevance',
-    'textFormat': 'plaintext',
-    'maxResults': 100,
-  }
-  if next_page_token is not None:
-    params['pageToken'] = next_page_token
-  response = requests.get(url + 'commentThreads', params=params)
-  resource = response.json()
-  comments = []
-
-  for comment_info in resource['items']:
-    # コメント
-    text = comment_info['snippet']['topLevelComment']['snippet']['textDisplay']
-    # # グッド数
-    # like_cnt = comment_info['snippet']['topLevelComment']['snippet']['likeCount']
-    # # 返信数
-    # reply_cnt = comment_info['snippet']['totalReplyCount']
-    # # ユーザー名
-    # user_name = comment_info['snippet']['topLevelComment']['snippet']['authorDisplayName']
-    # # Id
-    # parentId = comment_info['snippet']['topLevelComment']['id']
-    comments.append(text.replace('\r', '\n').replace('\n', ' '))
-    # comments.append(text)
-    # if reply_cnt > 0:
-    #   cno = 1
-    #   print_video_reply(no, cno, video_id, None, parentId)
-    no = no + 1
-
-  if 'nextPageToken' in resource:
-    print_video_comment(no, video_id, resource["nextPageToken"])
-  return comments
-
-def print_video_reply(no, cno, video_id, next_page_token, id,api_key):
-  params = {
-    'key': api_key,
-    'part': 'snippet',
-    'videoId': video_id,
-    'textFormat': 'plaintext',
-    'maxResults': 50,
-    'parentId': id,
-  }
-
-  if next_page_token is not None:
-    params['pageToken'] = next_page_token
-  url = os.environ.get("URL")
-  response = requests.get(url + 'comments', params=params)
-  resource = response.json()
-
-  for comment_info in resource['items']:
-    # コメント
-    text = comment_info['snippet']['textDisplay']
-    # グッド数
-    like_cnt = comment_info['snippet']['likeCount']
-    # ユーザー名
-    user_name = comment_info['snippet']['authorDisplayName']
-
-    cno = cno + 1
-
-  if 'nextPageToken' in resource:
-    print_video_reply(no, cno, video_id, resource["nextPageToken"], id,api_key)
-
-def get_comment(api_key,video_id,next_page_token):
-    # コメントを全取得する
-    print("get_comment")
-    no = 1
-    return print_video_comment(no, video_id, next_page_token)
+async def check_redirect(session, shorts_url, watch_url):
+    async with session.get(shorts_url, allow_redirects=True) as response:
+        if len(response.history) == 0:
+            return shorts_url
+        else:
+            return watch_url
