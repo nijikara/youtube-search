06fd6ca8504115463b96761c065b5b6feed6027a
nijikaraka <jmffn317@gmail.com>
2024-08-31 16:49:27 +0900

    パフォーマンス向上

--- get_comment_by_id.py
+++ get_comment_by_id.py
@@ -1,12 +1,16 @@
-import requests
+import aiohttp
+import asyncio
 import os
 from dotenv import load_dotenv
 import common
 
-async def get_comment_by_id(video_id):
-    load_dotenv('.env') 
+async def fetch(session, url, params):
+    async with session.get(url, params=params) as response:
+        return await response.json()
 
-    url = os.environ.get("URL")
+async def get_comment_by_id(video_id):
+    load_dotenv('.env')
+    url = os.environ.get("URL") + 'commentThreads'
     api_key = os.environ.get("API_KEY")
     params = {
         'key': api_key,
@@ -21,44 +25,43 @@ async def get_comment_by_id(video_id):
     no = 1
     next_page_token = None
 
-    while True:
-        if next_page_token is not None:
-            params['pageToken'] = next_page_token
-        response = requests.get(url + 'commentThreads', params=params)
-        resource = response.json()
+    async with aiohttp.ClientSession() as session:
+        while True:
+            if next_page_token:
+                params['pageToken'] = next_page_token
+            resource = await fetch(session, url, params)
 
-        for comment_info in resource['items']:
-            publishedAt = comment_info['snippet']['topLevelComment']['snippet']['publishedAt']
-            text = comment_info['snippet']['topLevelComment']['snippet']['textDisplay']
-            like_cnt = comment_info['snippet']['topLevelComment']['snippet']['likeCount']
-            reply_cnt = comment_info['snippet']['totalReplyCount']
-            user_name = comment_info['snippet']['topLevelComment']['snippet']['authorDisplayName']
-            parentId = comment_info['snippet']['topLevelComment']['id']
-            comments.append({
-                'no': str(no),
-                'publishedAt': common.change_time(publishedAt),
-                'comment': text,
-                'like_cnt': like_cnt,
-                'reply_cnt': reply_cnt,
-                'user_name': user_name,
-                'parentId': parentId + '-0',
-            })
+            for comment_info in resource['items']:
+                publishedAt = comment_info['snippet']['topLevelComment']['snippet']['publishedAt']
+                text = comment_info['snippet']['topLevelComment']['snippet']['textDisplay']
+                like_cnt = comment_info['snippet']['topLevelComment']['snippet']['likeCount']
+                reply_cnt = comment_info['snippet']['totalReplyCount']
+                user_name = comment_info['snippet']['topLevelComment']['snippet']['authorDisplayName']
+                parentId = comment_info['snippet']['topLevelComment']['id']
+                comments.append({
+                    'no': str(no),
+                    'publishedAt': common.change_time(publishedAt),
+                    'comment': text,
+                    'like_cnt': like_cnt,
+                    'reply_cnt': reply_cnt,
+                    'user_name': user_name,
+                    'parentId': parentId + '-0',
+                })
 
-            if reply_cnt > 0:
-                print(f"リプライ{reply_cnt}")
-                cno = 0
-                print_video_reply(no, cno, video_id, None, parentId, api_key, comments)
-            no = no + 1
+                if reply_cnt > 0:
+                    cno = 0
+                    await print_video_reply(no, cno, video_id, None, parentId, api_key, comments, session)
+                no += 1
 
-        if 'nextPageToken' in resource:
-            next_page_token = resource['nextPageToken']
-        else:
-            break
+            if 'nextPageToken' in resource:
+                next_page_token = resource['nextPageToken']
+            else:
+                break
 
     return comments
 
-def print_video_reply(no, cno, video_id, next_page_token, parentId, api_key, comments):
-    url = os.environ.get("URL")
+async def print_video_reply(no, cno, video_id, next_page_token, parentId, api_key, comments, session):
+    url = os.environ.get("URL") + 'comments'
     while True:
         params = {
             'key': api_key,
@@ -69,10 +72,9 @@ def print_video_reply(no, cno, video_id, next_page_token, parentId, api_key, com
             'parentId': parentId,
         }
 
-        if next_page_token is not None:
+        if next_page_token:
             params['pageToken'] = next_page_token
-        response = requests.get(url + 'comments', params=params)
-        resource = response.json()
+        resource = await fetch(session, url, params)
 
         for comment_info in resource['items']:
             publishedAt = comment_info['snippet']['publishedAt']
@@ -80,7 +82,7 @@ def print_video_reply(no, cno, video_id, next_page_token, parentId, api_key, com
             like_cnt = comment_info['snippet']['likeCount']
             user_name = comment_info['snippet']['authorDisplayName']
 
-            cno = cno + 1
+            cno += 1
             comments.append({
                 'no': str(no) + '-' + str(cno),
                 'publishedAt': common.change_time(publishedAt),
